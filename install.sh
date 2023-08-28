232c514dee2c500a3dc3987ab50022ca988dd6b6
EPMA <everton.araujo@grafeno.digital>
2023-08-28 02:55:00 -0300

    2nd

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 kubectl create namespace backstage
-kubectl apply -f postgres-deployment.yaml
-kubectl apply -f backstage-deployment.yaml
+kubectl apply -f postgres-secrets.yaml
+kubectl apply -f postgres-storage.yaml
+kubectl apply -f postgres-service.yaml
+kubectl apply -f postgres.yaml
+kubectl apply -f postgres-service.yaml
+kubectl apply -f backstage-secrets.yaml
+kubectl apply -f backstage-service.yaml
+#kubectl apply -f app-config.yaml
+kubectl port-forward --namespace=backstage svc/backstage 80:80
